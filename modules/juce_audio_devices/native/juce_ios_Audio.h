92add6c5a2913964779b699549f610094485f3dc
Mark Daunt <mark@wearepopgun.com>
2021-01-14 08:38:09 +1000

    mdd: upgrade to JUCE 6 and reapply SplashPro changes

--- modules/juce_audio_devices/native/juce_ios_Audio.h
+++ modules/juce_audio_devices/native/juce_ios_Audio.h
@@ -2,7 +2,7 @@
   ==============================================================================
 
    This file is part of the JUCE library.
-   Copyright (c) 2017 - ROLI Ltd.
+   Copyright (c) 2020 - Raw Material Software Limited
 
    JUCE is an open source library subject to commercial or open-source
    licensing.
@@ -74,13 +74,6 @@ public:
     Image getIcon (int size);
    #endif
     void switchApplication();
-    
-    static bool DidEngineStartFail()
-    {
-        auto ret = m_engineStartFailed;
-        m_engineStartFailed = false;
-        return ret;
-    }
 
 private:
     //==============================================================================
@@ -93,8 +86,6 @@ private:
     struct Pimpl;
     friend struct Pimpl;
     std::unique_ptr<Pimpl> pimpl;
-    
-    static bool m_engineStartFailed;
 
     JUCE_DECLARE_NON_COPYABLE (iOSAudioIODevice)
 };
--- modules/juce_gui_basics/windows/juce_DocumentWindow.cpp
+++ modules/juce_gui_basics/windows/juce_DocumentWindow.cpp
@@ -2,17 +2,16 @@
   ==============================================================================
 
    This file is part of the JUCE library.
-   Copyright (c) 2017 - ROLI Ltd.
+   Copyright (c) 2020 - Raw Material Software Limited
 
    JUCE is an open source library subject to commercial or open-source
    licensing.
 
-   By using JUCE, you agree to the terms of both the JUCE 5 End-User License
-   Agreement and JUCE 5 Privacy Policy (both updated and effective as of the
-   27th April 2017).
+   By using JUCE, you agree to the terms of both the JUCE 6 End-User License
+   Agreement and JUCE Privacy Policy (both effective as of the 16th June 2020).
 
-   End User License Agreement: www.juce.com/juce-5-licence
-   Privacy Policy: www.juce.com/juce-5-privacy-policy
+   End User License Agreement: www.juce.com/juce-6-licence
+   Privacy Policy: www.juce.com/juce-privacy-policy
 
    Or: You may also use this code under the terms of the GPL v3 (see
    www.gnu.org/licenses).
@@ -300,7 +299,7 @@ void DocumentWindow::lookAndFeelChanged()
         if ((requiredButtons & maximiseButton) != 0)  titleBarButtons[1].reset (lf.createDocumentWindowButton (maximiseButton));
         if ((requiredButtons & closeButton)    != 0)  titleBarButtons[2].reset (lf.createDocumentWindowButton (closeButton));
         if ((requiredButtons & customButton)   != 0)  titleBarButtons[3].reset (lf.createDocumentWindowButton (customButton));
-
+        
         for (auto& b : titleBarButtons)
         {
             if (b != nullptr)
